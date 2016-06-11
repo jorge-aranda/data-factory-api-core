add513414ead227b0d4f8ef7e9e593918098919f
jorge-aranda <jorgearanda123@gmail.com>
2016-06-11 14:34:46 +0200

    It works! : DataFactory.item("Hello").item(" ").item("World").item("!");

--- src/main/java/es/jaranda/datafactory/api/DataFactory.java
+++ src/main/java/es/jaranda/datafactory/api/DataFactory.java
@@ -2,6 +2,7 @@
 package es.jaranda.datafactory.api;
 
 import es.jaranda.datafactory.api.builder.CollectionFactory;
+import es.jaranda.datafactory.api.builder.impl.CollectionFactoryImpl;
 
 /**
  * Contains all methods to begin using API into static way
@@ -22,8 +23,7 @@ public class DataFactory {
      * @return CollectionFactory to still building your final collection
      */
     public static <T> CollectionFactory<T> item(T item) {
-        // TODO not implemented yet
-        throw new UnsupportedOperationException("Not supported yet");
+        final CollectionFactory<T> factory = new CollectionFactoryImpl<T>();
+        return factory.item(item);
     }
-    
 }
--- src/main/java/es/jaranda/datafactory/api/builder/impl/CollectionFactoryImpl.java
+++ src/main/java/es/jaranda/datafactory/api/builder/impl/CollectionFactoryImpl.java
@@ -2,6 +2,9 @@
 package es.jaranda.datafactory.api.builder.impl;
 
 import es.jaranda.datafactory.api.builder.CollectionFactory;
+import es.jaranda.datafactory.api.model.Node;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,16 +16,36 @@ import java.util.List;
  */
 public class CollectionFactoryImpl<T> implements CollectionFactory<T> {
     
+    private final Node<T> lastNode;
+    
+    public CollectionFactoryImpl() {
+        this(null);
+    }
+    
+    private CollectionFactoryImpl(Node<T> lastNode) {
+        this.lastNode = lastNode;
+    }
+    
     @Override
     public CollectionFactory<T> item(T item) {
-        // TODO not implemented yet
-        throw new UnsupportedOperationException("Not supported yet."); 
+        final Node<T> newNode = new Node(item, lastNode);
+        return new CollectionFactoryImpl<T>(newNode); 
     }
 
     @Override
     public List<T> toList() {
-        // TODO not implemented yet
-        throw new UnsupportedOperationException("Not supported yet.");
+        // TODO in future this method calls to another which can passed any kind of list
+        
+        final LinkedList<T> nodes = new LinkedList<T>();
+        Node<T> currentNode = lastNode;
+        
+        while(currentNode != null) {
+            nodes.add(currentNode.getValue());
+            currentNode = currentNode.getAncestor();
+        }
+        
+        Collections.reverse(nodes); // XXX this is a first version. It needs to improve algorithm
+        
+        return nodes;
     }
-    
 }
