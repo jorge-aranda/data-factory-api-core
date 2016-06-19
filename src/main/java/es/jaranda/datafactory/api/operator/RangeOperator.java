7adf4a04802e37e2c3f8871c3addc4fe73971984
jorge-aranda <jorgearanda123@gmail.com>
2016-06-19 17:50:22 +0200

    Improving specification and javadoc

--- src/main/java/es/jaranda/datafactory/api/operator/RangeOperator.java
+++ src/main/java/es/jaranda/datafactory/api/operator/RangeOperator.java
@@ -4,7 +4,14 @@ package es.jaranda.datafactory.api.operator;
 /**
  * Increase the next value of the range
  * @author Jorge
+ * @param <T> Type of range
  */
 public interface RangeOperator<T> {
-    T operate(T ancestor);
+    
+    /**
+     * Get next value in the range from current
+     * @param current Current favlue
+     * @return Next value in the range
+     */
+    T next(T current);
 }
